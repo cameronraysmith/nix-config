99175006c3ab59aa0068131c19ae7d45bc900f75
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-03 09:00:23 -0400

    fix(nix): enable auto-optimise-store
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -16,8 +16,9 @@
     nixPath = [ "nixpkgs=${flake.inputs.nixpkgs}" ]; # Enables use of `nix-shell -p ...` etc
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
     settings = {
+      auto-optimise-store = true;
       max-jobs = "auto";
-      experimental-features = "nix-command flakes repl-flake";
+      experimental-features = "nix-command flakes repl-flake auto-allocate-uids";
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
       trusted-users = [ "root" (if pkgs.stdenv.isDarwin then flake.config.people.myself else "@wheel") ];
