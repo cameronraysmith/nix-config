25e861f0857e5bd19b15e2815c2e122d352c43e8
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-05 22:48:40 -0400

    experiment: draft conda-lock derivation
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ packages/conda-lock.nix
@@ -0,0 +1,54 @@
+{ lib
+, python3Packages
+, fetchPypi
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "conda-lock";
+  version = "2.5.7";
+  format = "pyproject";
+
+  src = fetchPypi {
+    pname = "conda_lock";
+    inherit version;
+    hash = "sha256-3YXHYq2/biNf42VjByO0rOLX52DMrbomImM5A5DEmgY=";
+  };
+
+  nativeBuildInputs = with python3Packages; [ hatchling hatch-vcs ];
+
+  propagatedBuildInputs = with python3Packages; [
+    cachecontrol
+    cachy
+    click-default-group
+    click
+    clikit
+    crashtest
+    # ensureconda
+    gitpython
+    html5lib
+    jinja2
+    keyring
+    pkginfo
+    pydantic
+    pyyaml
+    requests
+    ruamel-yaml
+    tomlkit
+    toolz
+    # urllib3 # <2
+    virtualenv
+  ];
+
+  doCheck = false;
+  nativeCheckInputs = [ ];
+  disabledTests = [ ];
+  pythonImportsCheck = [ ];
+
+  meta = with lib; {
+    changelog = "https://github.com/conda/conda-lock/releases/tag/v${version}";
+    description = "Lightweight lockfile for conda environments";
+    homepage = "https://github.com/conda/conda-lock";
+    license = licenses.mit;
+    maintainers = with maintainers; [ cameronraysmith ];
+  };
+}
--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,6 +1,7 @@
 { flake, system, ... }:
 
 self: super: {
+  # conda-lock = self.callPackage ./conda-lock.nix { };
   nixci = flake.inputs.nixci.packages.${system}.default;
   quarto = self.callPackage ./quarto.nix { };
   # teller = self.callPackage ./teller.nix { };
