1a0ef555632707a4e54d6787a15fb58d47c31cc2
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-08 15:05:48 -0400

    fix(home): set extra-nix-path to flake
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/nix.nix
+++ home/nix.nix
@@ -8,7 +8,7 @@
       build-users-group = lib.mkDefault "nixbld";
       experimental-features = "nix-command flakes repl-flake auto-allocate-uids";
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
-      extra-nix-path = lib.mkDefault "nixpkgs=${flake.inputs.nixpkgs}";
+      extra-nix-path = lib.mkDefault "nixpkgs=flake:nixpkgs";
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
       max-jobs = "auto";
       trusted-users = [ "root" (if pkgs.stdenv.isDarwin then flake.config.people.myself else "@wheel") ];
