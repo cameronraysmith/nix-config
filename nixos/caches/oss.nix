f5d9858c1f8b10adf65f8529d3a6bf8703f84467
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-08 15:00:54 -0400

    fix(caches): update third party caches
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nixos/caches/oss.nix
+++ nixos/caches/oss.nix
@@ -1,10 +1,14 @@
 {
   nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cameronraysmith.cachix.org-1:aC8ZcRCVcQql77Qn//Q1jrKkiDGir+pIUjhUunN6aio="
-    #"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "poetry2nix.cachix.org-1:eXpeBJl0EQjO+vs9/1cUq19BH1LLKQT9HScbJDeeHaA="
   ];
   nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
     "https://cameronraysmith.cachix.org"
-    #"https://nix-community.cachix.org"
+    "https://poetry2nix.cachix.org"
   ];
 }
