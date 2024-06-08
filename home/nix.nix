a42e7d5372f66f90229621da3b68d3761282e51b
Cameron Smith <cameron.ray.smith@gmail.com>
2024-06-08 13:22:22 -0400

    fix(nix): sync home/nixos nixes
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/nix.nix
+++ home/nix.nix
@@ -5,10 +5,12 @@
     package = lib.mkDefault (pkgs.nix);
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
     settings = {
-      max-jobs = "auto";
+      build-users-group = lib.mkDefault "nixbld";
       experimental-features = "nix-command flakes repl-flake auto-allocate-uids";
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
+      extra-nix-path = lib.mkDefault "nixpkgs=${flake.inputs.nixpkgs}";
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
+      max-jobs = "auto";
       trusted-users = [ "root" (if pkgs.stdenv.isDarwin then flake.config.people.myself else "@wheel") ];
     };
   };
--- nixos/nix.nix
+++ nixos/nix.nix
@@ -17,10 +17,10 @@
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
     settings = {
       auto-optimise-store = true;
-      max-jobs = "auto";
       experimental-features = "nix-command flakes repl-flake auto-allocate-uids";
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
+      max-jobs = "auto";
       trusted-users = [ "root" (if pkgs.stdenv.isDarwin then flake.config.people.myself else "@wheel") ];
     };
   };
