6392b7bb832cdeefb00d20ded59cf1ef311cf000
Cameron Smith <cameron.ray.smith@gmail.com>
2024-07-13 15:09:19 -0400

    feat(home): update paths to automatically enable proto-managed tools
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/bash.nix
+++ home/bash.nix
@@ -3,7 +3,11 @@
     enable = true;
     initExtra = ''
       # Ensure all nix and home-manager installed files are available in PATH.
-      export PATH=/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/opt/homebrew/bin
+      export PROTO_HOME="$HOME/.proto"
+      export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
+      export PATH="/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
+      export PATH="$PATH:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin"
+      export PATH="$PATH:$HOME/.krew/bin:/opt/homebrew/bin"
     '';
   };
 }
