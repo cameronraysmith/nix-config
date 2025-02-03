31134d6b3002d3dea39501f1171bc9345ab0e71d
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-03 10:14:59 -0500

    fix(home): remove nix repl-flake
    https://www.github.com/NixOS/nix/issues/10103
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/nix.nix
+++ home/nix.nix
@@ -6,7 +6,7 @@
     registry.nixpkgs.flake = flake.inputs.nixpkgs; # Make `nix shell` etc use pinned nixpkgs
     settings = {
       build-users-group = lib.mkDefault "nixbld";
-      experimental-features = "nix-command flakes repl-flake auto-allocate-uids";
+      experimental-features = "nix-command flakes auto-allocate-uids";
       extra-platforms = lib.mkIf pkgs.stdenv.isDarwin "aarch64-darwin x86_64-darwin";
       extra-nix-path = lib.mkDefault "nixpkgs=flake:nixpkgs";
       flake-registry = builtins.toFile "empty-flake-registry.json" ''{"flakes":[],"version":2}'';
