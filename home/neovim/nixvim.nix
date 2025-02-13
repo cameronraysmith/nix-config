c8d3f8b37a6313682dd2ccd5ae67d64e8689c88b
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-13 00:44:12 -0500

    fix(nixvim): update theme integrations
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/neovim/nixvim.nix
+++ home/neovim/nixvim.nix
@@ -14,12 +14,10 @@
       lazydev-nvim
       persistence-nvim
       snacks-nvim
-      # catppuccin-nvim
     ];
 
     globals.mapleader = " ";
 
-    # colorscheme = "catppuccin";
     colorschemes.catppuccin = {
       enable = true;
       autoLoad = true;
@@ -27,18 +25,46 @@
 
     extraConfigLua = ''
       require("catppuccin").setup({
-        integrations = {
-          aerial = true,
-          gitsigns = true,
-          mason = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
+        opts = {
+          integrations = {
+            aerial = true,
+            alpha = true,
+            cmp = true,
+            dashboard = true,
+            flash = true,
+            fzf = true,
+            grug_far = true,
+            gitsigns = true,
+            headlines = true,
+            illuminate = true,
+            indent_blankline = { enabled = true },
+            leap = true,
+            lsp_trouble = true,
+            mason = true,
+            markdown = true,
+            mini = true,
+            native_lsp = {
+              enabled = true,
+              underlines = {
+                errors = { "undercurl" },
+                hints = { "undercurl" },
+                warnings = { "undercurl" },
+                information = { "undercurl" },
+              },
+            },
+            navic = { enabled = true, custom_bg = "lualine" },
+            neotest = true,
+            neotree = true,
+            noice = true,
+            notify = true,
+            semantic_tokens = true,
+            snacks = true,
+            telescope = true,
+            treesitter = true,
+            treesitter_context = true,
+            which_key = true,
           },
-          telescope = true,
-          treesitter = true,
-          which_key = true
-        }
+        },
       })
     '';
 
