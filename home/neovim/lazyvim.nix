cd0823ca9a839ad8c4de4b1c5bdfde0820bbc70f
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-13 03:03:06 -0500

    feat(lazyvim): enable aux plugins
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- home/neovim/lazyvim.nix
+++ home/neovim/lazyvim.nix
@@ -3,5 +3,48 @@
 {
   programs.lazyvim = {
     enable = true;
+    extras = {
+      coding = {
+        mini-surround.enable = true;
+      };
+      lang = {
+        # docker.enable = true;
+        json.enable = true;
+        # markdown.enable = true; # render-markdown-nvim
+        nix.enable = true;
+        # nushell.enable = true;
+        # ocaml.enable = true;
+        # python.enable = true;
+        # rust.enable = true;
+        # scala.enable = true;
+        # sql.enable = true;
+        # terraform.enable = true;
+        # toml.enable = true;
+        tailwind.enable = true;
+        typescript.enable = true;
+        # yaml.enable = true;
+      };
+      test = {
+        core.enable = true;
+      };
+      util = {
+        dot.enable = true;
+      };
+    };
+    plugins = with pkgs.vimPlugins; [
+      avante-nvim
+      copilot-lua
+      dressing-nvim
+      img-clip-nvim
+      mini-pick
+      nvim-web-devicons
+      render-markdown-nvim
+      telescope-nvim
+      telescope-fzf-native-nvim
+    ];
+    pluginsFile = {
+      "lazyvim.lua".source = ./lazyvim/lazyvim.lua;
+      "avante.lua".source = ./lazyvim/avante.lua;
+    };
   };
 }
