6697e363f5dbb38d00a1a686b967f9af6ce350f1
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-19 12:12:57 -0500

    refactor(flake): update for flake-parts module layout
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,170 +2,64 @@
   description = "Nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Principle inputs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixos-flake.url = "github:srid/nixos-flake";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nixos-unified.url = "github:srid/nixos-unified";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.url = "github:ryantm/agenix";
+    nuenv.url = "github:hallettj/nuenv/writeShellApplication";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    disko = {
-      url = "github:nix-community/disko";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    systems.url = "github:nix-systems/default";
-
-    omnix.url = "github:juspay/omnix";
-    nixci = {
-      url = "github:srid/nixci";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        systems.follows = "systems";
-      };
-    };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
+    # Software inputs
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs = {
         flake-parts.follows = "flake-parts";
         home-manager.follows = "home-manager";
         nix-darwin.follows = "nix-darwin";
-        # nixpkgs.follows = "nixpkgs";
-        treefmt-nix.follows = "treefmt-nix";
+        treefmt-nix.follows = "";
       };
     };
     lazyvim = {
       url = "github:matadaniel/LazyVim-module";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    omnix.url = "github:juspay/omnix";
     catppuccin.url = "github:catppuccin/nix";
+
+    # Development inputs
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.flake = false;
   };
 
-  outputs =
-    inputs @ { self
-    , ...
-    }:
+  outputs = inputs @ { self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.nixos-flake.flakeModule
-        ./users
-        ./home
-        ./nixos
-        ./nix-darwin
-      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      imports = with builtins;
+        map
+          (fn: ./modules/flake-parts/${fn})
+          (attrNames (readDir ./modules/flake-parts));
 
-      flake = {
-        darwinConfigurations.macbook-darwin =
-          self.nixos-flake.lib.mkMacosSystem
-            ./systems/darwin.nix;
-
-        nixosConfigurations.vm-nixos =
-          self.nixos-flake.lib.mkLinuxSystem
-            ./systems/nixos.nix;
-
-        nixosConfigurations.orb-nixos =
-          self.nixos-flake.lib.mkLinuxSystem
-            ./systems/orb;
+      perSystem = { lib, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = lib.attrValues self.overlays;
+          config.allowUnfree = true;
+        };
       };
 
-      perSystem = { self', pkgs, system, config, ... }:
-        let
-          users = [ "crs58" "jovyan" "runner" ];
-        in
-        {
-          legacyPackages.homeConfigurations = builtins.listToAttrs (map
-            (user: {
-              name = user;
-              value = self.nixos-flake.lib.mkHomeConfiguration
-                pkgs
-                ({ pkgs, ... }: {
-                  imports = [ self.homeModules.common ];
-                  home.username = user;
-                  home.homeDirectory =
-                    if user == "root"
-                    then "/root"
-                    else "/${
-                    if pkgs.stdenv.isDarwin
-                    then "Users"
-                    else "home"
-                    }/${user}";
-                });
-            })
-            users);
-
-          devShells = {
-            default = pkgs.mkShell {
-              inputsFrom = [ config.treefmt.build.devShell ];
-              packages = with pkgs; [
-                act
-                just
-                nixd
-                ratchet
-                teller
-              ];
-            };
-          };
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              (import ./packages/overlay.nix { inherit system; flake = { inherit inputs; }; })
-            ];
-          };
-
-          packages = {
-            default = self'.packages.activate;
-          };
-
-          # `nix run .#update` vs `nix flake update`
-          nixos-flake = {
-            primary-inputs = [
-              "nixpkgs"
-              "home-manager"
-              "nix-darwin"
-              "nix-index-database"
-              "nixvim"
-              "lazyvim"
-            ];
-          };
-
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              deno.enable = true;
-              nixpkgs-fmt.enable = true;
-              stylua.enable = true;
-              yamlfmt.enable = true;
-            };
-          };
-          formatter = config.treefmt.build.wrapper;
-
-        };
+      flake.om.ci.default.ROOT = {
+        dir = ".";
+        steps.flake-check.enable = false;
+        steps.custom = { };
+      };
     };
 }
