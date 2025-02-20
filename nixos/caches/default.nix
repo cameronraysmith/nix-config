668091c92d69c526b3e807705204e7eaf2fafebf
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-19 22:02:47 -0500

    refactor(nixos): deprecated
    - see `modules/nixos`
    
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nixos/caches/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  imports = [
-    ./oss.nix
-  ];
-}
--- nixos/current-location.nix
+++
@@ -1,8 +0,0 @@
-{
-  time.timeZone = "America/New_York";
-
-  location = {
-    latitude = 42.3643004;
-    longitude = -71.0911133;
-  };
-}
