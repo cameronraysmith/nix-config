2a14fedd982866560b331485dd15d3d1eaca5f2e
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-20 16:47:06 -0500

    test(lazyvim): add hmModule config
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ tests/lazyvim/home.nix
@@ -0,0 +1,41 @@
+{ inputs, pkgs, ... }: {
+  imports = [
+    inputs.lazyvim.homeManagerModules.default
+  ];
+
+  programs.git = {
+    enable = true;
+  };
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+  programs.lazyvim = {
+    enable = true;
+    # Apply minimal settings relative to our config in 
+    # ./modules/home/all/development/neovim/lazyvim.nix
+    extras = {
+      coding = {
+        blink.enable = true;
+        mini-surround.enable = true;
+      };
+      lang = {
+        nix.enable = true;
+        python.enable = true;
+      };
+    };
+    plugins = with pkgs.vimPlugins; [
+      # Core plugins from our main config
+      copilot-lua
+      mini-pick
+      telescope-nvim
+      telescope-fzf-native-nvim
+    ];
+    pluginsFile = {
+      # Use similar lazyvim.lua to our config
+      # ./modules/home/all/development/neovim/lazyvim/lazyvim.lua
+      "lazyvim.lua".source = ./lazyvim.lua;
+    };
+  };
+}
