f3656a43f2ee8e721de6ec74c87f8c6df246565d
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-22 15:13:33 -0500

    fix(packages): disable old overlay declarations
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,9 +1,9 @@
-{ flake, system, ... }:
+# { flake, system, ... }:
 
-self: super: {
-  # conda-lock = self.callPackage ./conda-lock.nix { };
-  nixci = flake.inputs.nixci.packages.${system}.default;
-  quarto = self.callPackage ./quarto.nix { };
-  omnix = flake.inputs.omnix.packages.${system}.default;
-  # teller = self.callPackage ./teller.nix { };
-}
+# self: super: {
+#   # conda-lock = self.callPackage ./conda-lock.nix { };
+#   nixci = flake.inputs.nixci.packages.${system}.default;
+#   quarto = self.callPackage ./quarto.nix { };
+#   omnix = flake.inputs.omnix.packages.${system}.default;
+#   # teller = self.callPackage ./teller.nix { };
+# }
