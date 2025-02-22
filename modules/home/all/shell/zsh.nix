9dab26f5f5f079d13d11494c142674b9daa9a943
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-22 14:54:16 -0500

    fix(zsh): disable proto env vars
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/shell/zsh.nix
+++ modules/home/all/shell/zsh.nix
@@ -7,8 +7,8 @@
 
     envExtra = ''
       # Ensure all nix and home-manager installed files are available in PATH.
-      export PROTO_HOME="$HOME/.proto"
-      export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
+      # export PROTO_HOME="$HOME/.proto"
+      # export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
       export PATH="/run/wrappers/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
       export PATH="$PATH:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin"
       export PATH="$PATH:$HOME/.krew/bin:/opt/homebrew/bin"
