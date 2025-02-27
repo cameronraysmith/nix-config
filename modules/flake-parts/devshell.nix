52bdb19aac38e4da987a0318077df71333861709
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-26 22:25:43 -0500

    fix(devshell): use nixfmt-rfc-style
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/flake-parts/devshell.nix
+++ modules/flake-parts/devshell.nix
@@ -3,24 +3,31 @@
   imports = [
     (inputs.git-hooks + /flake-module.nix)
   ];
-  perSystem = { inputs', config, pkgs, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "nix-config-shell";
-      meta.description = "Dev environment for nix-config";
-      inputsFrom = [ config.pre-commit.devShell ];
-      packages = with pkgs; [
-        just
-        nixd
-        nix-output-monitor
-        omnix
-        ratchet
-        teller
-        inputs'.agenix.packages.default
-      ];
-    };
+  perSystem =
+    {
+      inputs',
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "nix-config-shell";
+        meta.description = "Dev environment for nix-config";
+        inputsFrom = [ config.pre-commit.devShell ];
+        packages = with pkgs; [
+          just
+          nixd
+          nix-output-monitor
+          omnix
+          ratchet
+          teller
+          inputs'.agenix.packages.default
+        ];
+      };
 
-    pre-commit.settings = {
-      hooks.nixpkgs-fmt.enable = true;
+      pre-commit.settings = {
+        hooks.nixfmt-rfc-style.enable = true;
+      };
     };
-  };
-} 
+}
