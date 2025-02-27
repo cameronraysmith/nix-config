0efae33c30adcaae2b7a8ee1bb90d7e865fc76b8
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-26 22:29:16 -0500

    fix(packages): `overlay.nix` -> `overlays/default.nix`
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- packages/overlay.nix
+++
@@ -1,9 +0,0 @@
-# { flake, system, ... }:
-
-# self: super: {
-#   # conda-lock = self.callPackage ./conda-lock.nix { };
-#   nixci = flake.inputs.nixci.packages.${system}.default;
-#   quarto = self.callPackage ./quarto.nix { };
-#   omnix = flake.inputs.omnix.packages.${system}.default;
-#   # teller = self.callPackage ./teller.nix { };
-# }
