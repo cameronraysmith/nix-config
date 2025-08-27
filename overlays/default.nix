81f4115c1c52d1ac113ad030c8c99596629d07a9
Cameron Smith <cameron.ray.smith@gmail.com>
2025-08-27 12:12:52 -0400

    fix(overlays): enable markdown-tree-parser

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,6 +8,7 @@ in
 self: super: {
   # conda-lock = self.callPackage "${packages}/conda-lock.nix" { };
   # holos = self.callPackage "${packages}/holos.nix" { };
+  markdown-tree-parser = self.callPackage "${packages}/markdown-tree-parser.nix" { };
   omnix = inputs.omnix.packages.${self.system}.default;
   # quarto = self.callPackage "${packages}/quarto.nix" { };
   # star = self.callPackage "${packages}/star.nix" { };
