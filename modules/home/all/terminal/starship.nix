bb4ea52ab6159ad6a0d5af37059d86f5e2287965
Cameron Smith <cameron.ray.smith@gmail.com>
2025-08-28 15:44:13 -0400

    fix(terminal): enable starship starship-jj configuration
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/terminal/starship.nix
+++ modules/home/all/terminal/starship.nix
@@ -3,9 +3,24 @@
     enable = true;
     # catppuccin.enable = true;
     settings = {
+      format = ''
+        $all${custom.jj}$character
+      '';
       command_timeout = 2000;
       aws.disabled = true;
       gcloud.disabled = true;
+      custom.jj = {
+        command = "prompt";
+        format = "$output";
+        ignore_timeout = true;
+        shell = [
+          "starship-jj"
+          "--ignore-working-copy"
+          "starship"
+        ];
+        use_stdin = false;
+        when = true;
+      };
     };
   };
 }
