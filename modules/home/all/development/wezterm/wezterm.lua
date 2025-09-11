ebc42d1361952f6ace86a1cbba05302574a15c31
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-11 15:30:44 -0400

    fix(wezterm): update config
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/development/wezterm/wezterm.lua
+++ modules/home/all/development/wezterm/wezterm.lua
@@ -1,15 +1,17 @@
 return {
-  font = wezterm.font("Monaspace Neon"),
-  color_scheme = 'Catppuccin Mocha',
-  window_decorations = 'RESIZE',
-  keys = {
-    -- Emulate other programs (Zed, VSCode, ...)
-    {
-      key = 'P',
-      mods = 'CMD|SHIFT',
-      action = wezterm.action.ActivateCommandPalette,
-    },
-  },
-  -- Workaround for https://github.com/NixOS/nixpkgs/issues/336069#issuecomment-2299008280
-  -- front_end = "WebGpu"
+	font = wezterm.font("Monaspace Neon Nerd Font"),
+	color_scheme = "Catppuccin Mocha",
+	window_decorations = "RESIZE",
+	font_size = 13,
+	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		-- Emulate other programs (Zed, VSCode, ...)
+		{
+			key = "P",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.ActivateCommandPalette,
+		},
+	},
+	-- Workaround for https://github.com/NixOS/nixpkgs/issues/336069#issuecomment-2299008280
+	-- front_end = "WebGpu"
 }
