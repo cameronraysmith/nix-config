c2844292ef5ca832436e2721ea9d465b95402bad
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-13 15:55:32 -0400

    fix(home): explicitly set all rbw defaults
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/tools/rbw.nix
+++ modules/home/all/tools/rbw.nix
@@ -1,15 +1,37 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 {
   sops.secrets."BITWARDEN_EMAIL" = { };
 
+  # use sops template with overrides for rbw defaults
+  sops.templates."rbw-config" = {
+    content = builtins.toJSON {
+      email = config.sops.placeholder."BITWARDEN_EMAIL";
+      sso_id = null;
+      base_url = null;
+      identity_url = null;
+      ui_url = null;
+      notifications_url = null;
+      lock_timeout = 3600;
+      sync_interval = 3600;
+      pinentry = "pinentry";
+      client_cert_path = null;
+    };
+
+    path =
+      if pkgs.stdenv.isDarwin then
+        "${config.home.homeDirectory}/Library/Application Support/rbw/config.json"
+      else
+        "${config.xdg.configHome}/rbw/config.json";
+  };
+
   programs.rbw = {
     enable = true;
-    settings = {
-      email = builtins.readFile config.sops.secrets."BITWARDEN_EMAIL".path;
-    };
+    # nullify home-manager settings since we're using the sops template
+    settings = null;
   };
 }
