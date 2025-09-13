08b90e6944d3d40d0c1e81d0587f8ee349a03e80
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-13 15:51:43 -0400

    fix(home): hardcode defaultSopsFile path
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/core/sops.nix
+++ modules/home/all/core/sops.nix
@@ -14,5 +14,5 @@
   sops.age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
 
   # Set the default sops file location
-  sops.defaultSopsFile = "${self.outPath}/secrets/shared.yaml";
+  sops.defaultSopsFile = ../../../../secrets/shared.yaml;
 }
