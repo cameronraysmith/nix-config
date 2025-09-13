54db93b19ee057eed1b05d741b1950f928f3e8cc
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-13 14:37:19 -0400

    fix(home): use bw email
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/tools/rbw.nix
+++ modules/home/all/tools/rbw.nix
@@ -1,6 +1,18 @@
-{ pkgs, ... }:
 {
+  config,
+  pkgs,
+  self,
+  ...
+}:
+{
+  sops.secrets."BITWARDEN_EMAIL" = {
+    sopsFile = "${self}/secrets/shared.yaml";
+  };
+
   programs.rbw = {
     enable = true;
+    settings = {
+      email = builtins.readFile config.sops.secrets."BITWARDEN_EMAIL".path;
+    };
   };
 }
