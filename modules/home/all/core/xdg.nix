8e69e73d0decc37a523aeb4e7256dbffdf0bcf83
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-13 14:48:44 -0400

    feat(home): enable xdg config
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ modules/home/all/core/xdg.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{
+  # Enable XDG base directories for cross-platform compatibility
+  xdg.enable = true;
+
+  # This will set:
+  # https://github.com/nix-community/home-manager/blob/master/modules/misc/xdg.nix
+  # - Linux: ~/.config, ~/.cache, ~/.local/share, ~/.local/state
+  # - macOS: ~/Library/Application Support, ~/Library/Caches, etc.
+  # Following the XDG Base Directory Specification
+}
