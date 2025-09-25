9bd9b6668f426969f8fac4c655e22e819fb34ff8
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-25 17:49:29 -0400

    fix(overlays): remove LazyVim 6355 patch overlay
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,18 +14,4 @@ self: super: {
   # star = self.callPackage "${packages}/star.nix" { };
   starship-jj = self.callPackage "${packages}/starship-jj.nix" { };
   # teller = self.callPackage "${packages}/teller.nix" { };
-
-  # Temporary fix for LazyVim catppuccin integration issue
-  # https://github.com/LazyVim/LazyVim/issues/6355
-  # Remove once LazyVim PR#6354 is merged and available in nixpkgs
-  vimPlugins = super.vimPlugins // {
-    LazyVim = super.vimPlugins.LazyVim.overrideAttrs (oldAttrs: {
-      postPatch = (oldAttrs.postPatch or "") + ''
-        # Fix catppuccin bufferline integration - replace get() with get_theme()
-        substituteInPlace lua/lazyvim/plugins/colorscheme.lua \
-          --replace-fail 'require("catppuccin.groups.integrations.bufferline").get()' \
-                         'require("catppuccin.groups.integrations.bufferline").get_theme()'
-      '';
-    });
-  };
 }
