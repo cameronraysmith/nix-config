dfd0e8ee4817e209ed4f58a3c9505e84db9f5fb7
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-25 16:18:50 -0400

    fix(terminal): update atuin settings
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- modules/home/all/terminal/atuin.nix
+++ modules/home/all/terminal/atuin.nix
@@ -2,12 +2,18 @@
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+    # https://docs.atuin.sh/configuration/config/
     settings = {
       auto_sync = true;
+      sync.records = true;
+      dotfiles.enabled = true;
+      sync_frequency = "15m";
+      update_check = false;
       ctrl_n_shortcuts = false;
+      enter_accept = true;
       keymap_mode = "vim-insert";
       filter_mode_shell_up_key_binding = "directory";
-      search_mode = "fuzzy";
+      search_mode = "skim";
       show_help = false;
       show_preview = true;
       # Only necessary on certain file systems e.g. cephfs.
