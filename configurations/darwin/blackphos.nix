24e2df5c1aa2fef5ce31416ea834c8dc4849b973
Cameron Smith <cameron.ray.smith@gmail.com>
2025-09-27 17:56:01 -0400

    feat(blackphos): init
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ configurations/darwin/blackphos.nix
@@ -0,0 +1,40 @@
+{ flake, pkgs, ... }:
+
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    self.darwinModules.default
+  ];
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfree = true;
+
+  system.primaryUser = flake.config.me.username;
+
+  custom.homebrew = {
+    enable = true;
+    additionalCasks = [
+      "codelayer-nightly"
+      "dbeaver-community"
+      "docker-desktop"
+      "gpg-suite"
+      "inkscape"
+      "keycastr"
+      "meld"
+      "postgres-unofficial"
+    ];
+    additionalMasApps = {
+      save-to-raindrop-io = 1549370672;
+    };
+    manageFonts = false;
+  };
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
